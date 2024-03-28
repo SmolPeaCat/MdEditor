a2dd1375caf48fec2d1dc18347cd2776ba53572b
smolpeacat <smolpeacat@gmail.com>
2024-03-29 00:50:20 +0100

    Added Readme

--- SimpleMarkdownEditor/ViewModels/MarkdownParsingViewModel.cs
+++ SimpleMarkdownEditor/ViewModels/MarkdownParsingViewModel.cs
@@ -4,6 +4,7 @@ namespace SimpleMarkdownEditor.ViewModels;
 
 public partial class MarkdownParsingViewModel : ObservableObject
 {
+    // Enables any element binded to Text to change automatically 
     [ObservableProperty]
     private string? _text;
 }
